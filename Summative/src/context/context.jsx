afc972906702a2263e7bfe86aeafdb5b143bb853
Justin Su <justin.su348@gmail.com>
2025-05-24 00:07:35 -0400

    added firebase to context

--- Summative/src/context/context.jsx
+++ Summative/src/context/context.jsx
@@ -1,5 +1,7 @@
 import { createContext, useContext, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 import { Map } from "immutable";
+import { auth } from "../firebase";
 
 const StoreContext = createContext();
 
@@ -11,6 +13,23 @@ export const StoreProvider = ({ children }) => {
     const [cart, setCart] = useState(new Map());
     const [selectedGenres, setSelectedGenres] = useState(Map());
     const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+                console.log("Have a user");
+            } else {
+                setUser(null);
+                console.log("Do not have a user");
+            }
+            setLoading(false);
+        });
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <StoreContext.Provider value={{ name, setName, lastName, setLastName, email, setEmail, password, setPassword, cart, setCart, selectedGenres, setSelectedGenres, loggedIn, setLoggedIn }}>
